87cb491fe21a9eb357709df23c650c4211e2bbfb
sunwooKim <frod5@kakao.com>
2023-05-04 17:46:30 +0900

    자바 ORM 표준 JPA 프로그래밍 - 영속성 컨텍스트 flush(플러시) 실습

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -20,19 +20,46 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member findMember = em.find(Member.class, 3L);
-            findMember.setName("update3L");
+            Member member = new Member();
+            member.setId(10L);
+            member.setName("hello10L");
 
-            //em.persist(findMember)를 해야되지 않을까??? 보통 생각하기론..
-            //하지만 jpa는 find를 한 후 영속성컨텍스트에 저장된 후 .set으로 데이터를 변경한 후 commit을 하면 변경된 것을 감지하여 update쿼리를 날린다.
-            //변경감지(dirty checking)
+            em.persist(member);
 
-            //commit을하면 flush가 호출되고
-            //스냅샷은 영속성 컨텍스트에 첨에 들어간 시점의 엔티티이다.
-            //변경한 엔티티와 스냅샷을 비교한다.
-            //update쿼리를 쓰기지연 sql저장소에 둔다.
-            //commit할때 모아둔 쿼리가 커밋된다.
+            //영속성 컨텍스트의 변경내용을 데이터베이스에 반영
+            // em.flush()가 호출되면,
+            // 변경 감지, 수정된 엔티티 쓰기 지연 SQL 저장소에 등록, 쓰기 지연 SQL 저장소의 쿼리를 데이터베이스에 전송(등록, 수정, 삭제 쿼리)을 하게된다.
 
+            //flush를 하는 방법
+            //• em.flush() - 직접 호출
+            //• 트랜잭션 커밋(tx.commit()) - 플러시 자동 호출
+            //• JPQL 쿼리 실행 - 플러시 자동 호출
+
+            //flush를 하더라도 1차캐시는 유지된다.
+//            em.flush();
+
+            //em.setFlushMode(FlushModeType.COMMIT)
+            //• FlushModeType.AUTO 커밋이나 쿼리를 실행할 때 플러시 (기본값)
+            //• FlushModeType.COMMIT 커밋할 때만 플러시
+
+            //JPQL 쿼리 실행 - 플러시 자동 호출에 대한 설명
+            //em.persist(memberA)
+            //em.persist(memberB)
+            //em.persist(memberC)
+
+            //JPQL 쿼리 호출
+            //em.createQuery("select m from Member m", Member.class);
+            //List<Member> members= query.getResultList();
+
+            //이렇게하면 memberA,memberB,memberC는 기본적으로 flush()를 호출하지 않았지만,
+            //JPQL이 flush()를 호출하여, memberA,memberB,memberC에 대한 insert쿼리들이 날라간다.
+            //물론 커밋전에는 데이터베이스에 반영이 되지않는다. 커밋이 된다면 db반영.
+
+
+            //주의!
+            //• 영속성 컨텍스트를 비우지 않음
+            //• 영속성 컨텍스트의 변경내용을 데이터베이스에 동기화
+            //• 트랜잭션이라는 작업 단위가 중요 -> 커밋 직전에만 동기화 하면 됨
 
             //트랜잭션 커밋, 커밋을 해야 쿼리가 날라간다.
             tx.commit();
