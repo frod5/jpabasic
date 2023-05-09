68137a6311150cae72b09b521ee5680474f7686d
sunwooKim <frod5@kakao.com>
2023-05-09 15:47:02 +0900

    자바 ORM 표준 JPA 프로그래밍 - 다양한 연관관계 실전 실습

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "PARENT_ID")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> chlids = new ArrayList<>();
+
+    //실전에서 다대다는 중간 테이블을 이용해서 1:N, N:1로 해결.
+    //실전에서는 중간테이블이 단순하지 않다.
+    //실전에서는 ManyToMany 사용 x
+    @ManyToMany
+    @JoinTable(name = "CATEGORY_ITEM",
+            joinColumns = @JoinColumn(name = "CATEGORY_ID"),
+            inverseJoinColumns = @JoinColumn(name = "ITEM_ID")
+    )
+    private List<Item> items = new ArrayList<>();
+}
