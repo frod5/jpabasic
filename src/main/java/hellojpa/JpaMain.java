1bdf55821426bfe201f4a3c97010380eab5be905
sunwooKim <frod5@kakao.com>
2023-05-10 15:05:41 +0900

    자바 ORM 표준 JPA 프로그래밍 - 영속성전이(cascade) + orphanRemoval = true(고아객체) 실습

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -29,6 +29,26 @@ public class JpaMain {
 
             em.persist(parent);  // parent만 persist해도 child까지 persist된다. cascade = CascadeType.ALL
 
+            //영속성 전이: CASCADE - 주의!
+            //• 영속성 전이는 연관관계를 매핑하는 것과 아무 관련이 없음
+            //• 엔티티를 영속화할 때 연관된 엔티티도 함께 영속화하는 편리함을 제공할 뿐
+
+            em.flush();
+            em.clear();
+
+            //고아객체 - 참조가 제거된 엔티티는 다른 곳에서 참조하지 않는 고아객체로 보고 삭제하는 기능.
+            //참조하는곳이 하나일때 (Child가 Parent외에 다른곳에 쓰이면 안됨)
+            //특정 엔티티가 개인 소유할때 사용
+            //@OneToOne, @OneToMany만 사용 가능.
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);  // orphanRemoval = true설정시, delete쿼리가 나간다.
+
+            //영속성 전이 + 고아 객체, 생명주기
+            //• CascadeType.ALL + orphanRemoval=true
+            //• 스스로 생명주기를 관리하는 엔티티는 em.persist()로 영속화, em.remove()로 제거
+            //• 두 옵션을 모두 활성화 하면 부모 엔티티를 통해서 자식의 생명주기를 관리할 수 있음
+            //• 도메인 주도 설계(DDD)의 Aggregate Root개념을 구현할 때 유용
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
