b36e6c7e1196ff061a135d9af7ad0dd4e55b1c44
sunwooKim <frod5@kakao.com>
2023-05-11 18:12:32 +0900

    자바 ORM 표준 JPA 프로그래밍 - JPQL 경로 표현식

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -25,39 +26,29 @@ public class JpaMain {
             em.persist(team);
 
             Member2 member = new Member2();
-//            member.setUsername("member1");
-//            member.setUsername(null);
             member.setUsername("관리자");
             member.setAge(10);
             member.changeTeam(team);
             em.persist(member);
 
             Member2 member1 = new Member2();
-//            member.setUsername("member1");
-//            member.setUsername(null);
             member1.setUsername("관리자2");
             member1.setAge(10);
             member1.changeTeam(team);
             em.persist(member1);
 
+            //경로 표현식
+//            String query = "select m.username from Member2 m"; //상태 필드 - 경로 탐색의 끝, 탐색X
+//            String query = "select m.team from Member2 m"; //단일값 연관 경로 - 묵시적 내부조인(inner) 발생, 탐색O. select m.team.name 이런식으로
+            String query = "select m.username from Team2 t join t.members m"; //컬렉션 연관 경로 - 묵시적 내부조인(inner) 발생, 탐색X. from절에서 명시적 조인을 통해 별칭을 얻으면 별칭을 통해 탐색가능
 
-            //JPQL 함수 실습
-//            String query = "select 'a' || 'b' from Member2 m";  //hibernate 구현체로 하는 방법
-//            String query = "select concat('a', 'b') from Member2 m";  // concat()
-//            String query = "select substring(m.username,2,3) from Member2 m";  // substring()
-            //trim(), lower(), upper(), length() 등등..
+            //묵시적 조인은 사용하지 말자. 묵시적 조인은 항상 inner join
+            //명시적 조인 사용 - 명시적 조인은 직접 join문 작성.
 
-//            String query = "select locate('de','abcdefg') from Member2 m";  // locate() de가 들어있는 위치 반환 (Integer)
-//            String query = "select size(t.members) from Team2 t";  // size()
-            String query = "select function('group_concat', m.username) from Member2 m";  // 사용자 정의 함수 사용 MyH2Dialect 만들고, persistence.xml 수정 후. select group_concat(m.username) from Member2 m 도 hibernate구현체에서는 가능.
-
-            List<String> resultList = em.createQuery(query, String.class).getResultList();
-            for (String s : resultList) {
-                System.out.println("s = " + s);
+            Collection resultList = em.createQuery(query, Collection.class).getResultList();
+            for (Object o : resultList) {
+                System.out.println("o = " + o);
             }
-
-//            List<Member2> result = em.createQuery(query, Member2.class)
-//                    .getResultList();
 
             tx.commit();
         } catch (Exception e) {
