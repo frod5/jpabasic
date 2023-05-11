ed2c7ceb3800afe00100e116a744d641ff84c977
sunwooKim <frod5@kakao.com>
2023-05-11 11:00:08 +0900

    자바 ORM 표준 JPA 프로그래밍 - Native 쿼리 사용

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -23,30 +23,8 @@ public class JpaMain {
         tx.begin();
 
         try {
-            //criteria 사용 준비
-            CriteriaBuilder cb = em.getCriteriaBuilder();
-            CriteriaQuery<Member1> query = cb.createQuery(Member1.class);
-
-            Root<Member1> m = query.from(Member1.class);
-            CriteriaQuery<Member1> cq = query.select(m);
-
-            String username = "test";
-
-
-            //JPQL 빌더 역할
-            //JPA 공식 기능.
-            //동적 쿼리 사용이 편함.
-            //문자가 아닌 자바코드로 JPQL 작성 가능.
-            if(username != null) {
-                cq = cq.where(cb.equal(m.get("username"), "kim"));
-            }
-
-            List<Member1> resultList = em.createQuery(cq)
-                    .getResultList();
-
-            //단점 너무 보기가 불편한다.
-            //실무에서 잘 사용하지 않는다.
-            //criteria 보다는 QueryDSL 사용
+            //Native 쿼리
+            em.createNativeQuery("select MEMBER_ID, CITY, STREET, ZIPCODE FROM MBMBER1").getResultList();
 
             tx.commit();
         } catch (Exception e) {
