195a7ee440a1692f7f8739f2fbffd74a2fcc691f
sunwooKim <frod5@kakao.com>
2023-05-11 10:42:38 +0900

    자바 ORM 표준 JPA 프로그래밍 - JPQL

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,7 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -19,42 +20,8 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member1 member = new Member1();
-            member.setUsername("hello");
-            member.setHomeAddress(new Address1("homeCity","street","10000"));
-
-            member.getFavoriteFoods().add("치킨");
-            member.getFavoriteFoods().add("피자");
-            member.getFavoriteFoods().add("족발");
-
-            member.getAddressHistory().add(new AddressEntity("old1","street","10000"));
-            member.getAddressHistory().add(new AddressEntity("old2","street","10000"));
-
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            System.out.println("=============");
-            Member1 findMember = em.find(Member1.class, member.getId());  //값 타입 컬렉션 -> default 지연로딩
-
-            //치킨 -> 한식
-            findMember.getFavoriteFoods().remove("치킨");
-            findMember.getFavoriteFoods().add("한식");
-
-            //주소히스토리 변경
-            findMember.getAddressHistory().remove(new AddressEntity("old1","street","10000")); //컬렉션은 같은객체를 찾는것을 .equals로 찾기 떄문에 equals를 오버라이딩을 잘해주어야한다.
-            findMember.getAddressHistory().add(new AddressEntity("newCity1","street","10000"));
-
-            System.out.println("=============");
-
-            //값 타입 컬렉션은 영속성 전에(Cascade) + 고아 객체 제거 기능을 필수로 가진다고 볼 수 있다.
-            //값 타입 컬렉션의 제약사항
-            //• 값 타입은 엔티티와 다르게 식별자 개념이 없다.
-            //• 값은 변경하면 추적이 어렵다.
-            //• 값 타입 컬렉션에 변경 사항이 발생하면, 주인 엔티티와 연관된 모든 데이터를 삭제하고, 값 타입 컬렉션에 있는 현재 값을 모두 다시 저장한다.
-            //• 값 타입 컬렉션을 매핑하는 테이블은 모든 컬럼을 묶어서 기본키를 구성해야 함: null 입력X, 중복 저장X
-
+            List<Member1> resultList = em.createQuery("select m from Member1 m where m.username like '%kim%'", Member1.class)
+                    .getResultList();
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
